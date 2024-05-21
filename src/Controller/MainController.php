d9f5d86812b7549245020f17ba00729e71c0268d
Robin Delbaere <hello@rdelbaere.fr>
2024-05-21 14:43:18 +0200

    Home + debug-pack

---
+++ src/Controller/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('', name: 'main_')]
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('pages/main/index.html.twig');
+    }
+}
