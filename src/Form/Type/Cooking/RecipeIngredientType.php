44bbbec0cda92dd2b18d1172abe7578ad5cb528e
Robin Delbaere <hello@rdelbaere.fr>
2025-03-10 11:41:55 +0100

    Responsive for ingredient selection

--- src/Form/Type/Cooking/RecipeIngredientType.php
+++ src/Form/Type/Cooking/RecipeIngredientType.php
@@ -41,8 +41,11 @@ class RecipeIngredientType extends AbstractType
                 'class' => Ingredient::class,
                 'label' => false,
                 'choice_label' => 'label',
+                'row_attr' => [
+                    'class' => 'hidden',
+                ],
                 'attr' => [
-                    'class' => 'item-data-ingredient hidden',
+                    'class' => 'item-data-ingredient',
                 ],
             ]);
 
