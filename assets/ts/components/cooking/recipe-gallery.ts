a3cd00244bfc76bb6ba51f9cd4b6b005591dec56
Robin Delbaere <hello@rdelbaere.fr>
2025-03-21 16:12:39 +0100

    Recipe gallery renaming

--- assets/ts/components/cooking/recipe-gallery.ts
+++ assets/ts/components/cooking/recipe-gallery.ts
@@ -1,4 +1,4 @@
-import "@styles/components/cooking/recipe-list.scss";
+import "@styles/components/cooking/recipe-gallery.scss";
 import { AutocompleteEntity } from "../common/form/autocomplete-entity";
 
 AutocompleteEntity.init();
--- assets/ts/pages/cooking/recipe-explore/index.ts
+++ assets/ts/pages/cooking/recipe-explore/index.ts
@@ -1,2 +1,2 @@
 import "@styles/pages/cooking/recipe-explore/index.scss";
-import '../../../components/cooking/recipe-list'
+import '../../../components/cooking/recipe-gallery';
