b2228483e83a4a087b01da7a17c3b2eac7ddb66e
Robin Delbaere <hello@rdelbaere.fr>
2026-01-23 13:50:53 +0100

    Fix CS

--- src/Controller/User/SecurityController.php
+++ src/Controller/User/SecurityController.php
@@ -12,7 +12,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
