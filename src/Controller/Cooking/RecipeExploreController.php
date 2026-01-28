6fcd9a9d844989987a6f99d50d81856980f2a29d
Robin Delbaere <hello@rdelbaere.fr>
2026-01-28 11:25:22 +0100

    Rework explore controller

--- src/Controller/Cooking/RecipeExploreController.php
+++ src/Controller/Cooking/RecipeExploreController.php
@@ -16,83 +16,23 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[Route('/recette', name: 'cooking_recipe_explore_')]
 final class RecipeExploreController extends AbstractController
 {
-    public function __construct(private readonly RecipeRepository $recipeRepository)
-    {
+    public function __construct(
+        private readonly PaginationService $paginationService,
+        private readonly RecipeRepository $recipeRepository,
+    ) {
     }
 
     #[Route(name: 'index')]
-    public function index(Request $request, PaginationService $paginationService): Response
+    public function index(Request $request): Response
     {
-        $filter = new RecipeFilter();
-        $form = $this->createForm(RecipeFilterType::class, $filter);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $recipeQueryBuilder = $this->recipeRepository->findByFilterQueryBuilder($filter);
-        } else {
-            $recipeQueryBuilder = $this->recipeRepository->findByFilterQueryBuilder();
-        }
-
-        $offset = $request->query->getInt('offset');
-        $pagination = $paginationService->paginate($recipeQueryBuilder, $offset);
-
-        if ($request->isXmlHttpRequest()) {
-            return $this->json([
-                'status' => true,
-                'view' => $this->renderView('components/cooking/recipe-list.html.twig', [
-                    'recipes' => $pagination->getResults(),
-                ]),
-                'details' => [
-                    'offset' => $pagination->getOffset(),
-                    'limit' => $pagination->getLimit(),
-                    'total' => $pagination->getTotal(),
-                ],
-            ]);
-        }
-
-        return $this->render('pages/cooking/recipe-explore/index.html.twig', [
-            'form' => $form->createView(),
-            'pagination' => $pagination,
-        ]);
+        return $this->renderRecipes($request);
     }
 
     // TODO - Replace id by slug
     #[Route('/livre/{id:user}', name: 'book')]
-    public function book(Request $request, PaginationService $paginationService, User $user): Response
+    public function book(Request $request, User $user): Response
     {
-        $filter = new RecipeFilter();
-
-        $form = $this->createForm(RecipeFilterType::class, $filter);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $recipeQueryBuilder = $this->recipeRepository->findByBookAndFilterQueryBuilder($user, $filter);
-        } else {
-            $recipeQueryBuilder = $this->recipeRepository->findByBookAndFilterQueryBuilder($user);
-        }
-
-        $offset = $request->query->getInt('offset');
-        $pagination = $paginationService->paginate($recipeQueryBuilder, $offset);
-
-        if ($request->isXmlHttpRequest()) {
-            return $this->json([
-                'status' => true,
-                'view' => $this->renderView('components/cooking/recipe-list.html.twig', [
-                    'recipes' => $pagination->getResults(),
-                ]),
-                'details' => [
-                    'offset' => $pagination->getOffset(),
-                    'limit' => $pagination->getLimit(),
-                    'total' => $pagination->getTotal(),
-                ],
-            ]);
-        }
-
-        return $this->render('pages/cooking/recipe-explore/book.html.twig', [
-            'form' => $form->createView(),
-            'pagination' => $pagination,
-            'user' => $user,
-        ]);
+        return $this->renderRecipes($request, $user);
     }
 
     #[Route('/livre', name: 'my_book')]
@@ -101,6 +41,47 @@ final class RecipeExploreController extends AbstractController
     {
         return $this->forward('App\Controller\Cooking\RecipeExploreController::book', [
             'user' => $this->getUser(),
+        ]);
+    }
+
+    private function renderRecipes(Request $request, ?User $user = null): Response
+    {
+        $filter = new RecipeFilter();
+        $form = $this->createForm(RecipeFilterType::class, $filter);
+        $form->handleRequest($request);
+
+        $activeFilter = $form->isSubmitted() && $form->isValid() ? $filter : null;
+
+        if (null !== $user) {
+            $recipeQueryBuilder = $this->recipeRepository->findByBookAndFilterQueryBuilder($user, $activeFilter);
+        } else {
+            $recipeQueryBuilder = $this->recipeRepository->findByFilterQueryBuilder($activeFilter);
+        }
+
+        $offset = $request->query->getInt('offset');
+        $pagination = $this->paginationService->paginate($recipeQueryBuilder, $offset);
+
+        if ($request->isXmlHttpRequest()) {
+            return $this->json([
+                'status' => true,
+                'view' => $this->renderView('components/cooking/recipe-list.html.twig', [
+                    'recipes' => $pagination->getResults(),
+                ]),
+                'details' => [
+                    'offset' => $pagination->getOffset(),
+                    'limit' => $pagination->getLimit(),
+                    'total' => $pagination->getTotal(),
+                ],
+            ]);
+        }
+
+        $templateName = $user ? 'book' : 'index';
+        $templateExtraParameters = $user ? ['user' => $user] : null;
+
+        return $this->render(sprintf('pages/cooking/recipe-explore/%s.html.twig', $templateName), [
+            'form' => $form->createView(),
+            'pagination' => $pagination,
+            ...$templateExtraParameters ?? [],
         ]);
     }
 }
