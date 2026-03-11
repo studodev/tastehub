4f28dea7a75c199c029c5b59c37b049663011d39
Floriane Janicot <janicot.floriane@gmail.com>
2026-03-11 11:44:47 +0100

    add homepage entrypoint

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,8 @@ Encore
     .addEntry('app', './assets/ts/app.ts')
 
     /* ----- Pages entrypoints ---- */
+    .addEntry('main_index', './assets/ts/pages/main/index.ts')
+
     .addEntry('cooking_recipe_explore_index', './assets/ts/pages/cooking/recipe-explore/index.ts')
     .addEntry('cooking_recipe_explore_book', './assets/ts/pages/cooking/recipe-explore/book.ts')
 
