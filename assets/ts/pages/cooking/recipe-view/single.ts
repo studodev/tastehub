825932bc6e2888f100adc30229bd47d60f566a30
Robin Delbaere <hello@rdelbaere.fr>
2026-03-13 16:23:06 +0100

    Rework ingredient on step list

--- assets/ts/pages/cooking/recipe-view/single.ts
+++ assets/ts/pages/cooking/recipe-view/single.ts
@@ -1,9 +1,11 @@
 import "@styles/pages/cooking/recipe-view/single.scss";
+import { Expander } from "../../../components/common/expander";
 import { IncrementalNumber } from "../../../components/common/form/incremental-number";
 import { NumberWidget } from "../../../components/common/form/number-widget";
 import { TabNavigation } from "../../../components/common/tab-navigation";
 import { RecipeCustomizer } from "../../../components/cooking/recipe-customizer";
 
+Expander.init();
 IncrementalNumber.init();
 NumberWidget.init();
 RecipeCustomizer.init();
